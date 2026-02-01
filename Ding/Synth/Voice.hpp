4e2be803035916064b1cca4c8b66c97bd8bbaeed
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-02-01 03:33:43 +0100

    comment

--- Ding/Synth/Voice.hpp
+++ Ding/Synth/Voice.hpp
@@ -17,6 +17,9 @@ class SynthSound final : public juce::SynthesiserSound {
 class Voice final : public juce::SynthesiserVoice {
    public:
     Voice() = default;
+    // this is effectively the constructor
+    void setCurrentPlaybackSampleRate(double newRate) override;
+
     void renderNextBlock(AudioBuffer<float>& outputBuffer,
                          int startSample,
                          int numSamples) override;
@@ -26,8 +29,6 @@ class Voice final : public juce::SynthesiserVoice {
                    juce::SynthesiserSound* sound,
                    int /*pitchWheelPosition*/) override;
     void stopNote(float velocity, bool allowTailOff) override;
-
-    void setCurrentPlaybackSampleRate(double newRate) override;
 
     void pitchWheelMoved(int newPitchWheelValue) override;
     void controllerMoved(int controllerNumber, int newControllerValue) override;
