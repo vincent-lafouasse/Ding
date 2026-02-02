b50bb67671e7141574100c203241ef54236e8286
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-02-02 08:02:22 +0100

    hard inline the lerp call

--- Ding/core/DecibelLookup.cpp
+++ Ding/core/DecibelLookup.cpp
@@ -1,28 +1,20 @@
 #include "DecibelLookup.hpp"
 
-namespace {
-namespace impl {
-// t expected in [0, 1]
-float lerp(float t, float a, float b)
-{
-    return a + (b - a) * t;
-}
-}  // namespace impl
-}  // namespace
-
 float DecibelLookup::fromDb(float db)
 {
-    if (db < DecibelLookup::minDb) {
+    using DL = DecibelLookup;
+
+    if (db < DL::minDb) {
         return 0.0f;
-    } else if (db >= DecibelLookup::maxDb) {
-        return DecibelLookup::data[DecibelLookup::dataSize - 1];
+    } else if (db >= DL::maxDb) {
+        return DL::data[DL::dataSize - 1];
     }
 
-    const float floatIndex = (db + DecibelLookup::minDb) / DecibelLookup::step;
+    const float floatIndex = (db + DL::minDb) / DL::step;
 
     const std::size_t i = static_cast<std::size_t>(floatIndex);
     const float frac = floatIndex - static_cast<float>(i);
 
     // i is safe bc of the early checks
-    return impl::lerp(frac, DecibelLookup::data[i], DecibelLookup::data[i + 1]);
+    return DL::data[i] + frac * (DL::data[i + 1] - DL::data[i]);
 }
