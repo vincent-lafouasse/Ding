73da6abe1fde578ddf3463cec83e277e41a7ea14
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-02-02 05:40:58 +0100

    generate decibel lookup table

--- aux/generate_decibel_lookup_table.py
+++ aux/generate_decibel_lookup_table.py
@@ -1,18 +1,30 @@
 import math
 
 # Configuration
-FILENAME = "db_lut.inc"
+FILENAME = "./Ding/core/db_lut_data.cpp"
 MIN_DB = -96.0
 MAX_DB = 12.0
-N_PAGES = 2  # maybe 1 suffices but let's go at it
 
-N_ENTRIES = N_PAGES * 1024  # float[1024] takes 4KB
+N_ENTRIES = 2048  # 2 pages
 DB_RANGE = MAX_DB - MIN_DB
-STEP = DB_RANGE / (ENTRIES - 1)
+STEP = DB_RANGE / (N_ENTRIES - 1)
 
 
 def db_to_gain(db):
     return math.pow(10.0, db / 20.0)
 
 
-# with open(FILENAME, "w") as f:
+values = dict()
+
+for i in range(N_ENTRIES):
+    db = MIN_DB + i * STEP
+    values[db] = db_to_gain(db)
+
+
+with open(FILENAME, "w") as f:
+    f.write("#include <array>\n\n")
+    f.write(f"static constexpr std::array<float, {N_ENTRIES}> db_lut_data = {{\n")
+    for db in sorted(values):
+        gain = values[db]
+        f.write(f"    {gain:<15.8e}f, // {db:>7.2f} dB\n")
+    f.write("}\n")
