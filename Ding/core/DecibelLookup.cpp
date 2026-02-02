e9d53612dd5be61bc9bae07c0dcf9cf8cb507d7c
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-02-02 23:32:37 +0100

    minor refctor

--- Ding/core/DecibelLookup.cpp
+++ Ding/core/DecibelLookup.cpp
@@ -2,19 +2,19 @@
 
 float DecibelLookup::fromDb(float db)
 {
-    using DL = DecibelLookup;
+    using Self = DecibelLookup;
 
-    if (db < DL::minDb) {
+    if (db < Self::minDb) {
         return 0.0f;
-    } else if (db >= DL::maxDb) {
-        return DL::data[DL::dataSize - 1];
+    } else if (db >= Self::maxDb) {
+        return Self::data[Self::dataSize - 1];
     }
 
-    const float floatIndex = (db - DL::minDb) * DL::invStep;
+    const float floatIndex = (db - Self::minDb) * Self::invStep;
 
     const std::size_t i = static_cast<std::size_t>(floatIndex);
     const float frac = floatIndex - static_cast<float>(i);
 
     // i is safe bc of the early checks
-    return DL::data[i] + frac * (DL::data[i + 1] - DL::data[i]);
+    return Self::data[i] + frac * (Self::data[i + 1] - Self::data[i]);
 }
